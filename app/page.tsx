d21fa5918b2776bf9361875b6f8b280524cf4344
boris <boris@backed.by>
2024-03-29 11:55:23 -0700

    landing page mobile layout fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,13 @@ export default function Landing() {
         </div>
         <nav>
           <ul className="flex space-x-6">
-            <li>
-              <Button variant="ghost" size={"lg"} className="text-lg" asChild>
+            <li className="hidden md:block">
+              <Button
+                variant="ghost"
+                size={"lg"}
+                className="text-lg"
+                asChild
+              >
                 <NextLink href={"/about"}>About us</NextLink>
               </Button>
             </li>
@@ -35,42 +40,44 @@ export default function Landing() {
           </ul>
         </nav>
       </header>
-      <main className="flex flex-col items-center justify-center pt-48">
+      <main className="flex flex-col items-center justify-center pt-48 px-6 md:px-0">
         <div className="flex items-center justify-center mx-auto">
           <div className="text-left max-w-3xl mr-20">
             <h1 className="text-6xl font-bold mb-1">Introducing Quill:</h1>
             <h1 className="text-6xl font-bold mb-8">
               Effortless Documentation for the Modern Physician
             </h1>
-            <div className="rounded-lg">
-              <p className="text-3xl">
-                Quill is an AI scribe that listens to doctor-patient
-                interactions and writes accurate, complete notes. Replace the
-                computer and keyboard with better conversations and more time.
-              </p>
-              <p className="text-3xl mb-8">Built by doctors, for doctors.</p>
-              <Button className="py-3 px-6 text-xl" size={"lg"}>
-                Try Quill
+            <p className="text-3xl">
+              Quill is an AI scribe that listens to doctor-patient interactions
+              and writes accurate, complete notes. Replace the computer and
+              keyboard with better conversations and more time.
+            </p>
+            <p className="text-3xl mb-8">Built by doctors, for doctors.</p>
+            <div className="flex w-full justify-center md:justify-start">
+              <Button className="p-6 text-2xl" size={"lg"} asChild>
+                <NextLink href={"/signup"}> Try Quill</NextLink>
               </Button>
             </div>
           </div>
-          <div>
+          <div className="hidden md:block">
             <Image src="/doctor.svg" alt="Mascot" width={400} height={400} />
           </div>
         </div>
       </main>
       <section className="py-36 text-center">
-        <h2 className="text-4xl font-bold mb-8 text-center">How Quill Works</h2>
-        <div className="grid grid-cols-3 gap-8 max-w-5xl mx-auto">
+        <h2 className="text-4xl font-bold mb-12 text-center">How Quill Works</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-20 max-w-6xl mx-auto">
           <div>
             <h3 className="text-3xl font-bold mb-2">1. Consent</h3>
-            <p className="text-2xl mb-4">
-              {"Quill is introduced and everyone in the room agrees to let it scribe. Like a human scribe, Quill listens and doesn't record or save audio."}
+            <p className="text-2xl">
+              {
+                "Quill is introduced and everyone in the room agrees to let it scribe. Like a human scribe, Quill listens and doesn't save audio."
+              }
             </p>
           </div>
           <div>
             <h3 className="text-3xl font-bold mb-2">2. Listen</h3>
-            <p className="text-2xl mb-4">
+            <p className="text-2xl">
               The Quill mobile app listens to the conversation and writes the
               documentation based on the selected template.
             </p>
@@ -90,11 +97,11 @@ export default function Landing() {
           <table className="divide-y divide-gray-200 mx-auto">
             <tbody className="divide-y divide-gray-200">
               <tr>
-                <td className="px-6 py-4 text-3xl font-medium text-gray-900">
+                <td className="px-6 py-4 text-2xl md:text-3xl font-medium text-gray-900">
                   Robust Privacy and Security
                 </td>
                 <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-2xl">
+                  <ul className="list-disc pl-5 text-lg md:text-2xl">
                     <li>No audio is saved. No PHI included in notes.</li>
                     <li>Military-grade encryption and data handling</li>
                     <li>HIPAA compliant</li>
@@ -102,10 +109,10 @@ export default function Landing() {
                 </td>
               </tr>
               <tr>
-                <td className="px-6 py-4 text-3xl font-medium text-gray-900">
+                <td className="px-6 py-4 text-2xl md:text-3xl font-medium text-gray-900">
                   Intuitive and User-Friendly
                 </td>
-                <td className="px-6 py-4 text-2xl">
+                <td className="px-6 py-4 text-lg md:text-2xl">
                   <ul className="list-disc pl-5">
                     <li>Entire system designed by doctors to be intuitive</li>
                     <li>Seamless integration into existing workflows</li>
@@ -114,11 +121,11 @@ export default function Landing() {
                 </td>
               </tr>
               <tr>
-                <td className="px-6 py-4 text-3xl font-medium text-gray-900">
+                <td className="px-6 py-4 text-2xl md:text-3xl font-medium text-gray-900">
                   AI Superpowers
                 </td>
                 <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-2xl">
+                  <ul className="list-disc pl-5 text-lg md:text-2xl">
                     <li>Consistent and familiar documentation</li>
                     <li>Proven in noisy, fast-paced environments</li>
                     <li>Accurate over very long documents</li>
@@ -126,11 +133,11 @@ export default function Landing() {
                 </td>
               </tr>
               <tr>
-                <td className="px-6 py-4 text-3xl font-medium text-gray-900">
+                <td className="px-6 py-4 text-2xl md:text-3xl font-medium text-gray-900">
                   More time for...
                 </td>
                 <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-2xl">
+                  <ul className="list-disc pl-5 text-lg md:text-2xl">
                     <li>Better conversations</li>
                     <li>Trust and connection</li>
                     <li>Patient and physician wellness</li>
