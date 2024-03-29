5fdb14c0047f68040bb55d13174fbe07d5ba7355
boris <boris@backed.by>
2024-03-29 11:26:26 -0700

    removed theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,24 @@
-import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { EB_Garamond, Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const garamond = EB_Garamond({ subsets: ["latin"], variable: "--font-serif" });
 
 export const metadata: Metadata = {
-	title: "Quill",
-	description: "Effortless documentation with an AI scribe",
+  title: "Quill",
+  description: "Effortless documentation with an AI scribe",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={cn(inter.variable, garamond.variable)}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				>
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={cn(inter.variable, garamond.variable)}>{children}</body>
+    </html>
+  );
 }
