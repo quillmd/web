f32a9dc47ed5e7a79ebd2f9f76217f96a0fb776c
Saif Zaman <saif@Saifs-MacBook-Air.local>
2024-04-07 14:48:52 -0400

    Scroll snapping

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Landing() {
   // Throw an error for testing error handling
   // throw new Error('Test Error - Landing Page');
   return (
-    <div className="font-garamond overflow-x-hidden">
+    <div className="font-garamond overflow-x-hidden snap-y snap-mandatory h-screen overflow-scroll">
       <header className="fixed top-0 left-0 right-0 z-50 px-6 py-2 bg-background">
         <nav className="flex justify-between items-center w-full">
           <NextLink href={"/"}>
@@ -52,35 +52,37 @@ export default function Landing() {
           </ul>
         </nav>
       </header>
-      <main className="flex flex-col items-center justify-center pt-24 md:pt-48 px-4 md:px-0 max-w-full">
-        <div className="flex items-center justify-center mx-auto">
-          <div className="text-left md:text-left max-w-3xl mr-20">
-            <h1 className="text-5xl md:text-6xl font-bold mb-1">Introducing Quill:</h1>
-            <h1 className="text-5xl md:text-6xl font-bold mb-8">
-              Effortless Documentation for the Modern Physician
-            </h1>
-            <p className="text-2xl md:text-3xl">
-              Quill is an AI scribe that listens to doctor-patient interactions
-              and writes accurate, complete notes. Replace the computer and
-              keyboard with better conversations and more time.
-            </p>
-            <p className="text-2xl md:text-3xl mb-8">Built by doctors, for doctors.</p>
-            <div className="flex max-w-full justify-center md:justify-start">
-              <Button className="p-6 text-2xl" size={"lg"} asChild>
-                <NextLink href={"/signup"}> Try Quill</NextLink>
-              </Button>
+      <div className="h-screen snap-start">
+        <main className="flex flex-col items-center justify-center pt-24 md:pt-48 px-4 md:px-0 max-w-full h-full">
+          <div className="flex items-center justify-center mx-auto">
+            <div className="text-left md:text-left max-w-3xl mr-20">
+              <h1 className="text-5xl md:text-6xl font-bold mb-1">Introducing Quill:</h1>
+              <h1 className="text-5xl md:text-6xl font-bold mb-8">
+                Effortless Documentation for the Modern Physician
+              </h1>
+              <p className="text-2xl md:text-3xl">
+                Quill is an AI scribe that listens to doctor-patient interactions
+                and writes accurate, complete notes. Replace the computer and
+                keyboard with better conversations and more time.
+              </p>
+              <p className="text-2xl md:text-3xl mb-8">Built by doctors, for doctors.</p>
+              <div className="flex max-w-full justify-center md:justify-start">
+                <Button className="p-6 text-2xl" size={"lg"} asChild>
+                  <NextLink href={"/signup"}> Try Quill</NextLink>
+                </Button>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <Image src="/doctor.svg" alt="Mascot" width={400} height={400} />
             </div>
           </div>
-          <div className="hidden md:block">
-            <Image src="/doctor.svg" alt="Mascot" width={400} height={400} />
-          </div>
-        </div>
-      </main>
-      <section className="py-36 px-6 md:text-center">
+        </main>
+      </div>
+      <section className="py-36 px-6 md:text-center h-screen snap-start">
         <h2 className="text-4xl font-bold mb-12 text-center">
           How Quill Works
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-20 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-20 max-w-6xl mx-auto h-full">
           <div>
             <h3 className="text-3xl font-bold mb-2">1. Consent</h3>
             <p className="text-2xl">
@@ -105,89 +107,93 @@ export default function Landing() {
           </div>
         </div>
       </section>
-      <section className="max-w-full px-6">
-        <h2 className="text-4xl font-bold mb-6 text-center">Why Quill?</h2>
-        <div className="w-full">
-          <table className="divide-y divide-gray-200 mx-auto">
-            <tbody className="divide-y divide-gray-200">
-              <tr>
-                <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
-                  Robust Privacy and Security
-                </td>
-                <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-lg md:text-2xl">
-                    <li>No audio is saved. No PHI included in notes.</li>
-                    <li>Military-grade encryption and data handling</li>
-                    <li>HIPAA compliant</li>
-                  </ul>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
-                  Intuitive and User-Friendly
-                </td>
-                <td className="px-6 py-4 text-lg md:text-2xl">
-                  <ul className="list-disc pl-5">
-                    <li>Entire system designed by doctors to be intuitive</li>
-                    <li>Seamless integration into existing workflows</li>
-                    <li>No new hardware</li>
-                  </ul>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
-                  AI Superpowers
-                </td>
-                <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-lg md:text-2xl">
-                    <li>Consistent and familiar documentation</li>
-                    <li>Proven in noisy, fast-paced environments</li>
-                    <li>Accurate over very long documents</li>
-                  </ul>
-                </td>
-              </tr>
-              <tr>
-                <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
-                  More time for...
-                </td>
-                <td className="px-6 py-4">
-                  <ul className="list-disc pl-5 text-lg md:text-2xl">
-                    <li>Better conversations</li>
-                    <li>Trust and connection</li>
-                    <li>Patient and physician wellness</li>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-      <footer className="py-12 max-w-full">
-        <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <div>
-            <Image src="/hippa.svg" alt="HIPAA" width={120} height={120} />
+      <div className="h-screen snap-start">
+        <section className="max-w-full px-6 h-full flex flex-col justify-center">
+          <h2 className="text-4xl font-bold mb-6 text-center">Why Quill?</h2>
+          <div className="w-full">
+            <table className="divide-y divide-gray-200 mx-auto">
+              <tbody className="divide-y divide-gray-200">
+                <tr>
+                  <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
+                    Robust Privacy and Security
+                  </td>
+                  <td className="px-6 py-4">
+                    <ul className="list-disc pl-5 text-lg md:text-2xl">
+                      <li>No audio is saved. No PHI included in notes.</li>
+                      <li>Military-grade encryption and data handling</li>
+                      <li>HIPAA compliant</li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
+                    Intuitive and User-Friendly
+                  </td>
+                  <td className="px-6 py-4 text-lg md:text-2xl">
+                    <ul className="list-disc pl-5">
+                      <li>Entire system designed by doctors to be intuitive</li>
+                      <li>Seamless integration into existing workflows</li>
+                      <li>No new hardware</li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
+                    AI Superpowers
+                  </td>
+                  <td className="px-6 py-4">
+                    <ul className="list-disc pl-5 text-lg md:text-2xl">
+                      <li>Consistent and familiar documentation</li>
+                      <li>Proven in noisy, fast-paced environments</li>
+                      <li>Accurate over very long documents</li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-6 py-4 text-xl md:text-3xl font-medium text-gray-900">
+                    More time for...
+                  </td>
+                  <td className="px-6 py-4">
+                    <ul className="list-disc pl-5 text-lg md:text-2xl">
+                      <li>Better conversations</li>
+                      <li>Trust and connection</li>
+                      <li>Patient and physician wellness</li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <div className="flex-1 flex justify-center pr-12">
-            <ul className="flex space-x-6">
-              <li>
-                <a href="/privacypolicy" className="text-lg hover:underline">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a href="/contactus" className="text-lg hover:underline">
-                  Contact Us
-                </a>
-              </li>
-              <li>
-                <a href="/trustsafety" className="text-lg hover:underline">
-                  Trust & Safety
-                </a>
-              </li>
-            </ul>
+        </section>
+      </div>
+      <div className="snap-start">
+        <footer className="py-12 max-w-full">
+          <div className="max-w-7xl mx-auto flex items-center justify-between">
+            <div>
+              <Image src="/hippa.svg" alt="HIPAA" width={120} height={120} />
+            </div>
+            <div className="flex-1 flex justify-center pr-12">
+              <ul className="flex space-x-6">
+                <li>
+                  <a href="/privacypolicy" className="text-lg hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="/contactus" className="text-lg hover:underline">
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a href="/trustsafety" className="text-lg hover:underline">
+                    Trust & Safety
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
