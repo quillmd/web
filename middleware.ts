3bc3de41441075bec58047a61c3c0b600b82eadc
boris <boris@backed.by>
2024-04-08 22:16:37 -0700

    middleware fix attempt

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { validateUser } from "./lib/user";
 
 export function middleware(req: NextRequest) {
-  req.nextUrl;
   if (
     req.nextUrl.pathname == "/home" ||
     req.nextUrl.pathname.startsWith("/notes")
   ) {
     return validateUser().catch(() =>
-      Response.redirect(new URL("/login", req.nextUrl.origin))
+      NextResponse.redirect(new URL("/login", req.nextUrl.origin))
     );
   }
   if (req.nextUrl.pathname == "/login" || req.nextUrl.pathname == "/signup") {
     return validateUser()
-      .then(() => Response.redirect(new URL("/home", req.nextUrl.origin)))
+      .then(() => NextResponse.redirect(new URL("/home", req.nextUrl.origin)))
       .catch(() => null);
   }
 }
