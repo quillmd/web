9c8cc4436db18e1c51d6c91ab11bd9882d2c5527
boris <boris@backed.by>
2024-05-15 08:56:12 +0000

    improved case title ui

--- components/dashboard/case/case-title.tsx
+++ components/dashboard/case/case-title.tsx
@@ -1,22 +1,8 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { updateCase } from "@/lib/case";
-import {
-  ChangeCaseTitleFormSchema,
-  changeCaseTitleFormSchema,
-} from "@/lib/form-schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Edit, Save } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useDebounce } from "@/lib/useDebounce";
+import { SetStateAction, useState } from "react";
 
 interface CaseTitleProps extends React.HTMLAttributes<HTMLElement> {
   case_id: number;
@@ -24,65 +10,23 @@ interface CaseTitleProps extends React.HTMLAttributes<HTMLElement> {
 }
 
 export default function CaseTitle({ case_id, initial_title }: CaseTitleProps) {
-  const [isEditing, setIsEditing] = useState(false);
-  const form = useForm<ChangeCaseTitleFormSchema>({
-    resolver: zodResolver(changeCaseTitleFormSchema),
-    defaultValues: {
-      title: initial_title,
-    },
-  });
+  const [title, setTitle] = useState(initial_title);
 
-  function onSubmit(data: ChangeCaseTitleFormSchema) {
-    if (data.title != initial_title) {
-      updateCase({ id: case_id, title: data.title }).then(() => {
-        setIsEditing(false);
-        form.reset({ title: data.title });
-      });
-    } else {
-      setIsEditing(false);
-    }
-  }
-  const newTitle = form.watch("title");
+  const debouncedSet = useDebounce(
+    () => updateCase({ id: case_id, title: title }),
+    1000
+  );
+
+  const handleChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setTitle(e.target.value);
+    debouncedSet();
+  };
   return (
-    <div className="flex w-full">
-      {isEditing ? (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Case Title" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              variant={"ghost"}
-              type="submit"
-              disabled={newTitle.length == 0}
-            >
-              <Save />
-            </Button>
-          </form>
-        </Form>
-      ) : (
-        <>
-          <h2 className="text-3xl font-semibold tracking-tight transition-colors">
-            {initial_title}
-          </h2>
-          <Button
-            variant={"ghost"}
-            onClick={() => setIsEditing(!isEditing)}
-            disabled={newTitle.length == 0}
-          >
-            <Edit />
-          </Button>
-        </>
-      )}
-    </div>
+    <Input
+      value={title}
+      onChange={handleChange}
+      className="border-hidden hover:border-solid text-2xl font-semibold tracking-tight transition-colors shadow-none"
+      placeholder="Case Title"
+    />
   );
 }
